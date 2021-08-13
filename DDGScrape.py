e66568b490eb8772268ac998191208dc3eec3b97
josephquesnel <quesneljoseph@gmail.com>
2021-08-12 21:43:35 -0400

    generalizing downloader to all filetypes for input URL's, changed function names to reflect this

--- DDGScrape.py
+++ DDGScrape.py
@@ -1,4 +1,4 @@
-""" This Module uses selenium to get images and image url's from Duck Duck Go's image search. Only works with brave and chrome browser currently"""
+""" This Module uses selenium to get images and image url's from Duck Duck Go's image search. Can save any url of any image or file type. Only works with brave and chrome browser currently"""
 
 from urllib.parse import unquote
 from time import sleep
@@ -24,9 +24,9 @@ def search(query, brave=False):
     sleep(3)
 
     # For now it's working with this class, not sure if it will never change
-    img_tags = driver.find_elements_by_class_name('tile--img__img') 
+    file_tags = driver.find_elements_by_class_name('tile--file__img') 
 
-    for tag in img_tags:
+    for tag in file_tags:
         src = tag.get_attribute('data-src')
         src = unquote(src)
         src = src.split('=', maxsplit=1)
@@ -35,39 +35,41 @@ def search(query, brave=False):
     
     driver.close()
     
-def save_image(path, urls, image_name="image", original_name=False):
-    """ Saves image from url to path under the image name. A list of URL's is saved as image_name[i] where i is the index in the list
-        path works relative to current path or by absolute path, in both cases exclude the final backslash or the path wont work"""
+def save_file(path, urls, file_name="untitled", original_name=False):
+    """ Saves file from url to path under the original or chosen file name. If chosen, the file_name+index identifies the image
+        for path, include the final backslash or the path wont work"""
     
 
     # For individual img URL's
     if type(urls) == type(str()):
-        img_data = requests_get(urls).content
+        file_data = requests_get(urls).content
         if original_name==True:
-                image_name= findall("[0-9a-z.\-_%,&#]+.jpg", urls)[0][:-4]
-        with open(f'{path}{image_name}.jpg', 'wb') as handler:
-            handler.write(img_data)
+            filetype = urls[-4:]
+            file_name= findall(f"[0-9a-z.\-_%,&#]+{filetype}", urls)[0][:-4]
+        with open(f'{path}{file_name}{filetype}', 'wb') as handler:
+            handler.write(file_data)
             handler.close()
     
     # For lists of img URL's
     elif type(urls) in (type(list()), type(tuple())):
         for i, url in enumerate(urls):
-            img_data = requests_get(url).content
+            file_data = requests_get(url).content
             if original_name==True:
-                image_name= findall("[0-9a-z.\-_%,&#]+.jpg", url)[0][:-4]
+                filetype = url[-4:]
+                file_name= findall(f"[0-9a-z.\-_%,&#]+{filetype}", url)[0][:-4]
                 i=""
-            with open(f'{path}{image_name}{i}.jpg', 'wb') as handler:
-                handler.write(img_data)
+            with open(f'{path}{file_name}{i}{filetype}', 'wb') as handler:
+                handler.write(file_data)
                 handler.close()
     
     # Reject it if its not a tuple, list or string.
     else:
         raise TypeError("Error: Input URL format needs to be a string or a list of strings")
 
-def search_and_save(path, query, image_name="image", brave=False, original_name=False):
-    """All in one, Searches and then saves all images at chosen path with image_name as the base name, set brave to true if you use brave browser"""
+def search_and_save(path, query, file_name="image", brave=False, original_name=False):
+    """All in one, Searches and then saves all images at chosen path with file_name as the base name, set brave to true if you use brave browser"""
     imgs = list(search(query, brave=brave))
-    save_image(path, imgs, image_name=image_name, original_name=original_name)
+    save_file(path, imgs, file_name=file_name, original_name=original_name)
     
 if __name__ == '__main__':
     imgs_urls = list(search('face'))
